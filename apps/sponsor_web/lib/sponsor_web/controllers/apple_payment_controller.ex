6a13a358d405ec423911367d5d66f1e7598cee82
Fabian Keunecke <f@biankeunecke.de>
2023-12-07 14:45:07 +0100

    Decode & debug apple payload

--- apps/sponsor_web/lib/sponsor_web/controllers/apple_payment_controller.ex
+++ apps/sponsor_web/lib/sponsor_web/controllers/apple_payment_controller.ex
@@ -1,8 +1,19 @@
 defmodule Tankste.SponsorWeb.ApplePaymentController do
   use Tankste.SponsorWeb, :controller
 
-  def notify(conn, params) do
-    IO.inspect(conn)
-    IO.inspect(params)
+  def notify(conn, %{"signedPayload" => payload}) do
+    payload
+    |> Base.decode64!()
+    |> Poison.decode!()
+    |> IO.inspect
+
+    conn
+    |> send_resp(204, "")
+  end
+  def notify(conn, _) do
+    conn
+    |> put_status(422)
+    |> put_view(ChangesetView)
+    |> render("errors.json", validations: [signedPayload: {"invalid value", [validation: :invalid]}])
   end
 end
