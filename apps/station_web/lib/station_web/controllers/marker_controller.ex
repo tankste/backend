c7d812cc23c058340eb487a881b2596b46a3fdff
Fabian Keunecke <f@biankeunecke.de>
2024-01-22 22:39:33 +0100

    Add runtime option for marker list

--- apps/station_web/lib/station_web/controllers/marker_controller.ex
+++ apps/station_web/lib/station_web/controllers/marker_controller.ex
@@ -2,9 +2,28 @@ defmodule Tankste.StationWeb.MarkerController do
   use Tankste.StationWeb, :controller
 
   alias Tankste.Station.Markers
+  alias Tankste.Station.Markers.Marker
+  alias Tankste.Station.Stations
   alias Tankste.Station.OpenTimes
   alias Tankste.Station.StationAreas
   alias Tankste.Station.Holidays
+  alias Tankste.Station.Repo
+
+  @station_distance_comparing_meters 20_000 # 20 kilometers
+  @station_area_radius_degrees 0.3 # ~ 30 kilomters
+
+  def index(conn, %{"boundary" => boundary_param, "runtime" => "true"}) do
+    boundary = boundary_param
+      |> Enum.map(fn b -> b |> String.split(",") |> Enum.map(&String.to_float/1) end)
+      |> Enum.map(fn b -> List.to_tuple(b) end)
+
+    markers = Stations.list(boundary: boundary)
+      |> Repo.preload(:prices)
+      |> Enum.map(&gen_marker/1)
+      |> Enum.map(&override_states/1)
+
+    render(conn, "index.json", markers: markers)
+  end
 
   # TODO: limit requests to max ~0.2 degree
   # TODO: fall back request
@@ -70,4 +89,55 @@ defmodule Tankste.StationWeb.MarkerController do
   defp day(6), do: "saturday"
   defp day(7), do: "sunday"
   defp day(_), do: nil
+
+  # Runtime calculation parts
+
+  defp gen_marker(station) do
+    near_stations = Stations.list(boundary: [{station.location_latitude - @station_area_radius_degrees, station.location_longitude - @station_area_radius_degrees}, {station.location_latitude + @station_area_radius_degrees, station.location_longitude + @station_area_radius_degrees}])
+    |> Enum.filter(fn s -> Geocalc.within?(@station_distance_comparing_meters, [s.location_longitude, s.location_latitude], [station.location_longitude, station.location_latitude]) end)
+    |> Repo.preload(:prices)
+
+    %Marker{
+      station_id: station.id,
+      label: station.brand || station.name,
+      latitude: station.location_latitude,
+      longitude: station.location_longitude,
+      e5_price: get_price(station, "e5"),
+      e5_price_comparison: get_price_comparison(station, "e5", near_stations),
+      e10_price: get_price(station, "e10"),
+      e10_price_comparison: get_price_comparison(station, "e10", near_stations),
+      diesel_price: get_price(station, "diesel"),
+      diesel_price_comparison: get_price_comparison(station, "diesel", near_stations)
+    }
+  end
+
+  defp get_price(station, type) do
+    case Enum.find(station.prices, fn p -> p.type == type end) do
+    nil ->
+      nil
+    price ->
+      price.price
+    end
+  end
+
+  defp get_price_comparison(station, type, near_stations) do
+    near_prices = near_stations
+      |> Enum.flat_map(fn s -> s.prices end)
+      |> Enum.filter(fn p -> p.type == type end)
+      |> Enum.map(fn p -> p.price end)
+
+    case get_price(station, type) do
+      nil ->
+        "not_available"
+      price_value ->
+        min_price = near_prices
+          |> Enum.min()
+
+        cond do
+          min_price + 0.04 >= price_value -> "cheap"
+          min_price + 0.10 >= price_value -> "medium"
+          true -> "expensive"
+        end
+    end
+  end
 end
