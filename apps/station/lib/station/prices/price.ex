2a7d456ad951766a3a6c3ec4e1f1fca2cc78ea97
Fabian Keunecke <f@biankeunecke.de>
2024-04-19 15:04:58 +0200

    Add more fuel types

--- apps/station/lib/station/prices/price.ex
+++ apps/station/lib/station/prices/price.ex
@@ -18,9 +18,20 @@ defmodule Tankste.Station.Prices.Price do
 
   def changeset(station, attrs) do
     station
-    |> cast(attrs, [:station_id, :origin_id, :type, :price, :last_changes_at])
+    |> cast(attrs, [:station_id, :origin_id, :type, :price, :last_changes_at, :priority])
     |> validate_required([:station_id, :origin_id, :type, :price])
-    |> validate_inclusion(:type, ~w(e5 e10 diesel))
+    # petrol: Benzin (91 Oktan)
+    # (e5) petrol_super_e5: Benzin E5 (95 Oktan)
+    # (e10) petrol_super_e10: Benzin E10 (95 Oktan)
+    # petrol_super_plus: Benzin (98 Oktan)
+    # petrol_shell_power: Shell V-Power Racing (100 Oktan)
+    # petrol_aral_ultimate: Aral Ultimate (102 Oktan)
+    # diesel: Diesel (PKW)
+    # diesel_truck: Diesel (LKW)
+    # diesel_shell_power: Shell V-Power Diesel
+    # diesel_aral_ultimate: Ultimate Diesel von Aral
+    # lpg: Autogas
+    |> validate_inclusion(:type, ~w(petrol e5 e10 petrol_super_plus petrol_shell_power petrol_aral_ultimate diesel diesel_truck diesel_shell_power diesel_aral_ultimate lpg))
     |> unique_constraint([:station_id, :type])
   end
 end
