7aa8a0328665223abe9f90c9483b4ea2e502964b
Fabian Keunecke <f@biankeunecke.de>
2024-04-20 17:31:16 +0200

    Soft migration clean

--- apps/station/priv/repo/migrations/20240420132435_remove_old_station_indexes.exs
+++ apps/station/priv/repo/migrations/20240420132435_remove_old_station_indexes.exs
@@ -2,8 +2,8 @@ defmodule Tankste.Station.Repo.Migrations.RemoveOldStationIndexes do
   use Ecto.Migration
 
   def up do
-    execute "ALTER TABLE station_info_areas DROP CONSTRAINT station_areas_station_id_fkey"
-    execute "ALTER TABLE station_info_open_times DROP CONSTRAINT station_open_times_station_id_fkey"
+    execute "ALTER TABLE station_info_areas DROP IF EXISTS CONSTRAINT station_areas_station_id_fkey"
+    execute "ALTER TABLE station_info_open_times DROP IF EXISTS CONSTRAINT station_open_times_station_id_fkey"
   end
 
   def down do
