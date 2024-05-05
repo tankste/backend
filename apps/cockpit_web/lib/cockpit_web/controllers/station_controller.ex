55492106039ed64143f0957c9a9000bdb322279a
Fabian Keunecke <f@biankeunecke.de>
2024-05-05 13:47:14 +0200

    Add station list pagination

--- apps/cockpit_web/lib/cockpit_web/controllers/station_controller.ex
+++ apps/cockpit_web/lib/cockpit_web/controllers/station_controller.ex
@@ -4,12 +4,36 @@ defmodule Tankste.CockpitWeb.StationController do
   alias Tankste.Station.Repo
   alias Tankste.Station.StationInfos
 
-  def index(conn, _params) do
-    station_infos = StationInfos.list()
+  # TODO: clean up paging logic, too messy output today
+  def index(conn, params) do
+    all_station_infos = StationInfos.list()
     |> Enum.sort_by(fn si -> si.priority end, :desc)
     |> Enum.uniq_by(fn si -> si.station_id end)
-    |> Repo.preload([:station])
 
-    render(conn, :index, station_infos: station_infos)
+    page = Map.get(params, "page", "0") |> String.to_integer()
+    page_start = page * 50
+    page_end = page_start + 49
+
+    page_station_infos = all_station_infos |> Enum.slice(page_start..page_end) |> Repo.preload([:station])
+    page_count = (page_station_infos |> length())
+    stations_count = all_station_infos |> length()
+
+    render(
+      conn,
+      :index,
+      page_station_infos: page_station_infos,
+      stations_count: stations_count,
+      page_start: page_start,
+      page_end: page_start + page_count - 1,
+      previous_page: case page do
+          0 -> nil
+          _ -> page - 1
+        end,
+      next_page: case page_start + page_count do
+          ^stations_count ->
+            nil
+          _ -> page + 1
+        end
+    )
   end
 end
