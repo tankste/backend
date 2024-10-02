292edf75d1160901bda366dd93cf2a5cd5577d13
Fabian Keunecke <f@biankeunecke.de>
2024-10-02 12:35:41 +0200

    Add report details page

--- apps/cockpit_web/lib/cockpit_web/controllers/report_controller.ex
+++ apps/cockpit_web/lib/cockpit_web/controllers/report_controller.ex
@@ -4,6 +4,9 @@ defmodule Tankste.CockpitWeb.ReportController do
   import Tankste.CockpitWeb.AuthPlug
 
   alias Tankste.Report.Reports
+  alias Tankste.Station.Stations
+  alias Tankste.Station.StationInfos
+  alias Tankste.Station.Repo
 
   plug :load_current_user
   plug :require_current_user
@@ -11,5 +14,12 @@ defmodule Tankste.CockpitWeb.ReportController do
   def index(conn, _params) do
     reports = Reports.list(sort: [{:desc, :inserted_at}])
     render(conn, :index, reports: reports)
+  end
+
+  def show(conn, %{"id" => id}) do
+    report = Reports.get(id)
+    station = Stations.get(report.station_id)
+    station_info = StationInfos.get_by_station_id(report.station_id)
+    render(conn, :show, report: report, station: station, station_info: station_info)
   end
 end
